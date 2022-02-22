6d2705e44c27a74cec7f6169a8de65a13ae5c66c
Harrison Hoyos <harrihoyos2680@gmail.com>
2022-02-21 21:44:16 -0500

    Newton Raphson v5

--- Newton_Raphson.m
+++ Newton_Raphson.m
@@ -1,14 +1,14 @@
-function [V, delta] = Newton_Raphson( Y_barra, datos_potencia, init, Sb)
+function [V, delta, count] = Newton_Raphson( Y_barra, datos_potencia, init, Sb)
     datos_potencia(datos_potencia(:,9) == false,9) = init;
     V = datos_potencia(:,9);
     delta = datos_potencia(:,10);
-    Pcalc = zeros(length(Y_barra(2:end,1)),1); Qcalc = zeros(length(Y_barra(2:end,1)),1);
     datos_potencia(:,3:8) = datos_potencia(:,3:8)./Sb;
     Pprog = datos_potencia(2:end,3)-datos_potencia(2:end,5);
     Qprog = datos_potencia(2:end,4)-datos_potencia(2:end,6);
     error = 1e-3;
     count = 1;
     while true
+        Pcalc = zeros(length(Y_barra(2:end,1)),1); Qcalc = zeros(length(Y_barra(2:end,1)),1);
         for i = 1:length(Y_barra(2:end,1))
             for j = 1:length(Y_barra(1,:))
                 Pcalc(i) = Pcalc(i) + abs(Y_barra(i+1,j)*V(i+1)*V(j))*cos(angle(Y_barra(i+1,j))+delta(j)-delta(i+1));
@@ -22,30 +22,31 @@ function [V, delta] = Newton_Raphson( Y_barra, datos_potencia, init, Sb)
         Qcalc_PV = Qcalc(datos_potencia(2:end,2) == 1);
         limits = datos_potencia(datos_potencia(:,2)==1,7:8);
         posiciones_limits = find(datos_potencia(:,2) == 1);
+        Qd_PV = datos_potencia(datos_potencia(:,2)==1,4);
         deltas = [deltaP;deltaQ];
         if count > 1
             for i = 1:length(limits(:,1))
                 try
-                    if Qcalc_PV(i) < limits(i,1)
+                    if Qcalc_PV(i) + Qd_PV(i) < limits(i,1)
                         datos_potencia(posiciones_limits(i), 2) = 2;
                         pos = find((Qcalc == Qcalc_PV(i))==1);
                         datos_potencia(pos,4) = limits(i,1);
                         Qprog(pos) = limits(i,1);
                         deltaQ(pos) = Qprog(pos) - Qcalc(pos);
-                    elseif Qcalc_PV(i) > limits(i,2)
+                    elseif Qcalc_PV(i) + Qd_PV(i) > limits(i,2)
                         datos_potencia(posiciones_limits(i), 2) = 2;
                         pos = find((Qcalc == Qcalc_PV(i))==1);
                         Qprog(pos) = limits(i,2);
                         deltaQ(pos) = Qprog(pos) - Qcalc(pos);
                     end
                 catch ME
-                    if Qcalc_PV < limits(i,1)
+                    if Qcalc_PV + Qd_PV < limits(i,1)
                         datos_potencia(posiciones_limits, 2) = 2;
                         pos = find((Qcalc == Qcalc_PV)==1);
                         datos_potencia(pos,4) = limits(i,1);
                         Qprog(pos) = limits(i,1);
                         deltaQ(pos) = Qprog(pos) - Qcalc(pos);
-                    elseif Qcalc_PV(i) > limits(i,2)
+                    elseif Qcalc_PV(i) + Qd_PV > limits(i,2)
                         datos_potencia(posiciones_limits(i), 2) = 2;
                         pos = find((deltaQ == Qcalc_PV)==1);
                         datos_potencia(pos,4) = limits(i,2);
@@ -57,11 +58,6 @@ function [V, delta] = Newton_Raphson( Y_barra, datos_potencia, init, Sb)
         end
         delta_del = deltaQ(datos_potencia(2:end,2) == 1);
         deltaQ1 = deltaQ(~(datos_potencia(2:end,2) == 1));
-        if ~isempty(delta_del)
-            pos_del = find((deltas == delta_del)==1);
-        else
-            pos_del = [];
-        end
         H = []; N = []; L = []; M = [];
         for i = 2:length(Y_barra(:,1))
             for j = 2:length(Y_barra(1,:))
@@ -79,36 +75,24 @@ function [V, delta] = Newton_Raphson( Y_barra, datos_potencia, init, Sb)
             end
         end
         Jacob = [H N;M L];
-        if ~isempty(pos_del)
-            for i = 1:length(pos_del)
-                try
-                    Jacob(pos_del(i),:) = 0;
-                    Jacob(:,pos_del(i)) = 0;
-                catch ME
-                    Jacob(pos_del,:) = 0;
-                    Jacob(:,pos_del) = 0;
-                end
-            end
+        if ~isempty(delta_del)
+            iaux2 = logical([zeros(size(deltaP));datos_potencia(2:end,2)==1]);
+            Jacob = Jacob(~iaux2,~iaux2);
         end
         deltas_pot = [deltaP;deltaQ1];
-        if deltas_pot < error
-            break
-        end
-        if ~isempty(pos_del)
-            Jacob = reshape(Jacob(Jacob~=0), [], length(deltas_pot));
-        end
-        disp(Jacob)
-        if count == 2
-            break
-        end
         deltas_v = inv(Jacob)*deltas_pot;
         deltav_v = deltas_v(length(deltaP)+1:end);
         del_v = deltav_v.*datos_potencia(datos_potencia(2:end,2)~=1,9);
-        vs = datos_potencia(datos_potencia(2:end,2)~=1,9)+del_v;
-        V(datos_potencia(1:end,2)== 2) = vs;
-        delta(2:end) = deltas_v(1:length(deltaP));  
+        V(datos_potencia(1:end,2)== 2) = V(datos_potencia(1:end,2)== 2)+del_v;
+        delta(2:end) = deltas_v(1:length(deltaP))+delta(2:end); 
+        if abs(deltas_pot) < error
+            break
+        else
+            disp(deltas_pot)
+        end
         count = count + 1;
-    end 
+    end
+    count = count-1;
 end
 
 
